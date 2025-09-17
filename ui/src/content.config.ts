f43e4279a844a6b332267c169d80b2e8e1cfdb8c
Lev Chelyadinov <leva181777@gmail.com>
2025-09-17 23:38:46 +0200

    Implement data extraction from Git

---
+++ ui/src/content.config.ts
@@ -0,0 +1,124 @@
+import type { Loader } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+import { x } from "tinyexec";
+
+function productLoader({
+  branch = "archive",
+  pathPrefix = "products/",
+} = {}): Loader {
+  const schema = z.object({
+    firstAppearance: z.string().date(),
+    absenceRanges: z.array(
+      z.object({
+        start: z.string().date(),
+        end: z.string().date(),
+      })
+    ),
+  });
+
+  return {
+    name: "product-loader",
+    schema,
+    load: async ({ store, parseData }) => {
+      const { stdout: allProductsEverStdout } = await x(
+        "git",
+        ["log", branch, "--pretty=format:", "--name-only", "--diff-filter=A"],
+        { nodeOptions: { cwd: ".." } }
+      );
+
+      const allProductsEver = [...new Set(allProductsEverStdout.split("\n"))]
+        .filter((line) => line.length > 0)
+        .map((line) => {
+          if (line.startsWith('"') && line.endsWith('"')) {
+            return line.slice(1, -1).replaceAll('\\"', '"');
+          } else {
+            return line;
+          }
+        });
+
+      const batchSize = 1000;
+      for (
+        let productIndex = 0;
+        productIndex < allProductsEver.length;
+        productIndex += batchSize
+      ) {
+        const batch = allProductsEver.slice(
+          productIndex,
+          productIndex + batchSize
+        );
+
+        await Promise.all(
+          batch.map(async (productPath) => {
+            const { stdout: addedRemovedEventsStdout } = await x(
+              "git",
+              [
+                "log",
+                "--oneline",
+                "--diff-filter=AD",
+                branch,
+                "--",
+                productPath,
+              ],
+              { nodeOptions: { cwd: ".." } }
+            );
+
+            const addedRemovedEvents = addedRemovedEventsStdout
+              .trim()
+              .split("\n")
+              .map((line) => line.split(" ")[1]);
+
+            const firstAppearance = addedRemovedEvents.pop();
+            const absenceRanges = addedRemovedEvents
+              .reverse()
+              .reduce<z.infer<typeof schema>["absenceRanges"]>(
+                (ranges, event, index, array) => {
+                  if (index % 2 === 1) {
+                    ranges.push({ start: array[index - 1], end: event });
+                  } else if (index === array.length - 1) {
+                    ranges.push({
+                      start: event,
+                      end: new Date()
+                        .toISOString()
+                        .slice(0, "yyyy-mm-dd".length),
+                    });
+                  }
+                  return ranges;
+                },
+                []
+              );
+
+            const id = productPath.replace(pathPrefix, "").replace(".yaml", "");
+            if (id.length === 0) {
+              return;
+            }
+
+            const product = {
+              id,
+              data: {
+                firstAppearance,
+                absenceRanges,
+              },
+            };
+
+            try {
+              store.set({ id, data: await parseData(product) });
+            } catch (e) {
+              console.error(`Error parsing ${productPath}:`, {
+                product,
+                addedRemovedEventsStdout,
+                productPath,
+              });
+              throw e;
+            }
+          })
+        );
+      }
+    },
+  };
+}
+
+const products = defineCollection({
+  loader: productLoader(),
+});
+
+export const collections = { products };
